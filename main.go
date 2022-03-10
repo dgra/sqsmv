5d07e7819f72fa87155a82fa1bf266688304e6c6
Dustin Grange <dustin@grange.io>
2022-03-10 16:56:53 -0700

    Support fifo queue transfers as well.
    This does not support Standard to FIFO or FIFO to Standard.
    Only Standard to Standard or FIFO to FIFO.

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	src := flag.String("src", "", "source queue")
 	dest := flag.String("dest", "", "destination queue")
 	clients := flag.Int("clients", 1, "number of clients")
+	fifo := flag.Bool("fifo", false, "is this a fifo queue?")
 	flag.Parse()
 
 	if *src == "" || *dest == "" || *clients < 1 {
@@ -25,6 +26,7 @@ func main() {
 	log.Printf("source queue : %v", *src)
 	log.Printf("destination queue : %v", *dest)
 	log.Printf("number of clients : %v", *clients)
+	log.Printf("is this a fifo queue? : %v", *fifo)
 
 	// enable automatic use of AWS_PROFILE like awscli and other tools do.
 	opts := session.Options{
@@ -39,26 +41,56 @@ func main() {
 	maxMessages := int64(10)
 	waitTime := int64(0)
 	messageAttributeNames := aws.StringSlice([]string{"All"})
+	attributeNames := aws.StringSlice([]string{"All"})
 
 	rmin := &sqs.ReceiveMessageInput{
 		QueueUrl:              src,
 		MaxNumberOfMessages:   &maxMessages,
 		WaitTimeSeconds:       &waitTime,
 		MessageAttributeNames: messageAttributeNames,
+		AttributeNames:        attributeNames,
 	}
 
 	var wg sync.WaitGroup
 	for i := 1; i <= *clients; i++ {
 		wg.Add(i)
-		go transferMessages(sess, rmin, dest, &wg)
+		go transferMessages(sess, rmin, dest, fifo, &wg)
 	}
 	wg.Wait()
 
 	log.Println("all done")
 }
 
+//buildFifoMessageInput parsed out the message group id and deduplication id and
+//builds a SendMessageInput suitable for a FIFO destination
+func buildFifoMessageInput(m *sqs.Message, dest *string) sqs.SendMessageInput {
+	messageGroupID := m.Attributes["MessageGroupId"]
+	messageDeduplicationID := m.Attributes["MessageDeduplicationId"]
+
+	log.Printf("MessageGroupId: %s", *messageGroupID)
+	log.Printf("MessageDeduplicationId: %s", *messageDeduplicationID)
+	// write the message to the destination queue
+	return sqs.SendMessageInput{
+		MessageAttributes:      m.MessageAttributes,
+		MessageBody:            m.Body,
+		QueueUrl:               dest,
+		MessageGroupId:         messageGroupID,
+		MessageDeduplicationId: messageDeduplicationID,
+	}
+}
+
+//buildStandardMessageInput builds a SendMessageInput suitable for a Standard queue destination
+func buildStandardMessageInput(m *sqs.Message, dest *string) sqs.SendMessageInput {
+	// write the message to the destination queue
+	return sqs.SendMessageInput{
+		MessageAttributes: m.MessageAttributes,
+		MessageBody:       m.Body,
+		QueueUrl:          dest,
+	}
+}
+
 //transferMessages loops, transferring a number of messages from the src to the dest at an interval.
-func transferMessages(theSession *session.Session, rmin *sqs.ReceiveMessageInput, dest *string, wgOuter *sync.WaitGroup) {
+func transferMessages(theSession *session.Session, rmin *sqs.ReceiveMessageInput, dest *string, fifo *bool, wgOuter *sync.WaitGroup) {
 	client := sqs.New(theSession)
 
 	lastMessageCount := int(1)
@@ -68,6 +100,8 @@ func transferMessages(theSession *session.Session, rmin *sqs.ReceiveMessageInput
 	// loop as long as there are messages on the queue
 	for {
 		resp, err := client.ReceiveMessage(rmin)
+
+		log.Printf("msg: %v", resp.Messages)
 
 		if err != nil {
 			panic(err)
@@ -89,11 +123,11 @@ func transferMessages(theSession *session.Session, rmin *sqs.ReceiveMessageInput
 			go func(m *sqs.Message) {
 				defer wg.Done()
 
-				// write the message to the destination queue
-				smi := sqs.SendMessageInput{
-					MessageAttributes: m.MessageAttributes,
-					MessageBody:       m.Body,
-					QueueUrl:          dest,
+				var smi sqs.SendMessageInput
+				if *fifo {
+					smi = buildFifoMessageInput(m, dest)
+				} else {
+					smi = buildStandardMessageInput(m, dest)
 				}
 
 				_, err := client.SendMessage(&smi)
